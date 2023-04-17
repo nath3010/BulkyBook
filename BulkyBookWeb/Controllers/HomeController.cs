c137e66678a1f3a6c8424f32200d04e84c51b9c1
nathalia <nathalia.dias3010@gmail.com>
2023-04-16 21:47:49 -0300

    first files

---
+++ BulkyBookWeb/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace BulkyBookWeb.Controllers
+{   
+    //controller Home,  dentro de views tem uma pasta com o mesmo nome
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //action, na pasta Home dentro de view tem um arquivo com o mesmo nome
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //action, na pasta Home dentro de view tem um arquivo com o mesmo nome
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BulkyBookWeb.Data
+{   
+    //DbContext é do pacote entityframework core
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)//passando para a classe bae
+        {   
+            
+        }
+
+        //para criar a tabela dentro do banco de dados
+        public DbSet<Category> Categories { get; set; }
+    }
+}
